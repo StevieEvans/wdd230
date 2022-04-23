68178cd6f26261d7092df0be2bc63c652b256ae2
StevieEvans <eva17024@byui.edu>
2022-04-23 15:27:04 -0600

    update

---
+++ js/index.js
@@ -0,0 +1,2 @@
+document.getElementById('last-modified').innerHTML = "<p> Last Updated: " + document.lastModified + "</p>"
+document.getElementById('curret-year').innerHTML = new Date().getFullYear()
