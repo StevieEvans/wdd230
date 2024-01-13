e3ba1fa41f19742ce1cdfdba2ef1d2b80e3c9d2f
StevieEvans <eva17024@byui.edu>
2024-01-13 16:16:55 -0700

    W01 Course Home Page

--- scripts/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('last-modified').innerHTML = "<p> Last Updated: " + document.lastModified + "</p>"
-document.getElementById('current-year').innerHTML = new Date().getFullYear()
-
-// h1.innerHTML = 'Steve Evans'
-
-// const today1 = document.querySelector('#today1')
-
-// let today = new Date()
