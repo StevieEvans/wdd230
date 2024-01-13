baa362ecc64fcb2da82238faf95395a887dce851
StevieEvans <eva17024@byui.edu>
2024-01-13 13:13:11 -0700

    lesson 1 holy grail

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+document.getElementById('last-modified').innerHTML = "<p> Last Updated: " + document.lastModified + "</p>"
+document.getElementById('current-year').innerHTML = new Date().getFullYear()
+
+// h1.innerHTML = 'Steve Evans'
+
+// const today1 = document.querySelector('#today1')
+
+// let today = new Date()
