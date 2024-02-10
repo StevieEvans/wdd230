a03a881784e7adb5cee5be288c8eb453b50d43dc
StevieEvans <svans516@gmail.com>
2024-02-09 19:52:17 -0700

    W05 Chamber Work

---
+++ chamber/scripts/darkmode.js
@@ -0,0 +1,12 @@
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("body");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.checked == true) {
+		main.style.background = "#000";
+		main.style.color = "#fff";
+	} else {
+		main.style.background = "#eee";
+		main.style.color = "#000";
+	}
+});
--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,6 +1,2 @@
-// toLocaleDateString
-// const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
-// document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
-
 document.getElementById('last-modified').textContent = `Last Updated: ${document.lastModified}` 
 document.getElementById('current-year').textContent = new Date().getFullYear()
