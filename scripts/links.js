1fae87f7eace4083ba26b587f4376cfbbc2c34ca
StevieEvans <svans516@gmail.com>
2024-07-01 18:24:39 -0600

    fix vars

--- scripts/links.js
+++ scripts/links.js
@@ -1,10 +1,15 @@
 const baseURL = "stevieevans.github.io/wdd230/";
 const linksURL = "stevieevans.github.io/wdd230/data/links.json"
 
-getLinks = async () => {
+const getLinks = async () => {
     const response = await fetch(linksURL)
-    data = response.json()
+    const data = await response.json()
     console.log(data)
+    // displayLinks(data.lessons)
+}
+
+const displayLinks = (lessons) => {
+
 }
 
 getLinks()
